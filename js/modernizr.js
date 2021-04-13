e432a8d86d6066524b8bd517e242da07bf302ef3
Andrew Sykes <andrewsykes@btinternet.com>
2021-04-13 09:28:20 +0100

    no shapes fallback

--- js/modernizr.js
+++ js/modernizr.js
@@ -2934,6 +2934,26 @@ Method of allowing calculated values for length units. For example:
     return !!el.style.length;
   });
     
+    /*!
+{
+  "name": "CSS Shapes",
+  "property": "shapes",
+  "tags": ["css"],
+  "notes": [{
+    "name": "CSS Shapes W3C specification",
+    "href": "https://www.w3.org/TR/css-shapes"
+  },{
+    "name": "Examples from Adobe",
+    "href": "http://webplatform.adobe.com/shapes/"
+  }, {
+    "name": "Samples showcasing uses of Shapes",
+    "href": "http://codepen.io/collection/qFesk"
+  }]
+}
+!*/
+
+  Modernizr.addTest('shapes', testAllProps('shapeOutside', 'content-box', true));
+    
       
 /*!
 {
